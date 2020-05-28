1213e7d5cdc6bdad2a6cecae312afaa2c3210227
jatv93 <toledojennifer20@gmail.com>
2020-05-28 18:23:15 +0000

    primer commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,29 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-export function Home() {
+export const Home = ({
+	secondsDer,
+	secondsIzq,
+	minutesDer,
+	minutesIzq,
+	hoursDer,
+	hoursIzq
+}) => {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container">
+				<div className="clock">
+					<i className="far fa-clock" />
+				</div>
+				<div className="col">{hoursIzq}</div>
+				<div className="col">{hoursDer}</div>
+				<p>:</p>
+				<div className="col">{minutesIzq}</div>
+				<div className="col">{minutesDer}</div>
+				<p>:</p>
+				<div className="col">{secondsIzq}</div>
+				<div className="col">{secondsDer}</div>
+			</div>
+		</>
 	);
-}
+};
--- src/js/index.js
+++ src/js/index.js
@@ -11,5 +11,55 @@ import "../styles/index.scss";
 //import your own components
 import { Home } from "./component/home.js";
 
+let secondsDer = 0;
+let secondsIzq = 0;
+let minutesDer = 0;
+let minutesIzq = 0;
+let hoursDer = 0;
+let hoursIzq = 0;
+
+setInterval(() => {
+	secondsDer += 1;
+	if (secondsDer == 10) {
+		secondsDer = 0;
+		secondsIzq++;
+	}
+
+	if (secondsIzq == 6) {
+		secondsIzq = 0;
+		minutesDer++;
+	}
+
+	if (minutesDer == 10) {
+		minutesDer = 0;
+		minutesIzq++;
+	}
+
+	if (minutesIzq == 6) {
+		minutesIzq = 0;
+		hoursDer++;
+	}
+
+	if (hoursDer == 10) {
+		hoursDer = 0;
+		hoursIzq++;
+	}
+
+	if (hoursIzq == 10) {
+		hoursIzq = 0;
+	}
+
+	ReactDOM.render(
+		<Home
+			secondsDer={secondsDer}
+			secondsIzq={secondsIzq}
+			minutesDer={minutesDer}
+			minutesIzq={minutesIzq}
+			hoursDer={hoursDer}
+			hoursIzq={hoursIzq}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
